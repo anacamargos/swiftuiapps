e2f2a9ce0554667d010f9d57a13200e29987a361
anacamargos <alcvlara@sga.pucminas.br>
2022-01-11 11:30:20 -0300

    Adding GuessTheFlag

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Ana Leticia Camargos on 10/01/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
