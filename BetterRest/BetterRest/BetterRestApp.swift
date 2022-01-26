37d33974832e1384f3806cec61529c03c28ec0b1
anacamargos <alcvlara@sga.pucminas.br>
2022-01-26 09:06:59 -0300

    Creating a new project

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Ana Leticia Camargos on 26/01/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
