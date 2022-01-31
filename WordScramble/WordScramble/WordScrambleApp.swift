b90cc43ff4b3fdab93c22ef388b8f59a54d0c2d2
anacamargos <alcvlara@sga.pucminas.br>
2022-01-31 08:54:48 -0300

    Creating new project

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Ana Leticia Camargos on 31/01/22.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
