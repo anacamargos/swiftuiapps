6a6e4297a4c80aabae1465674d750b00d7705f96
anacamargos <alcvlara@sga.pucminas.br>
2022-02-15 10:37:57 -0300

    Creating new project

---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Ana Leticia Camargos on 15/02/22.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
