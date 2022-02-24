1b5fa18b3bd37b64eeca45d1764cc32242b0cb9d
anacamargos <alcvlara@sga.pucminas.br>
2022-02-24 11:39:18 -0300

    Showing missions on screen

--- Moonshot/Moonshot/Bundle-Decodable.swift
+++ Moonshot/Moonshot/Bundle-Decodable.swift
@@ -16,7 +16,11 @@ extension Bundle {
         guard let data = try? Data(contentsOf: url) else {
             fatalError("Failed to load \(file) in bundle.")
         }
-        guard let loaded = try? JSONDecoder().decode(T.self, from: data) else {
+        let decoder = JSONDecoder()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "y-MM-dd"
+        decoder.dateDecodingStrategy = .formatted(formatter)
+        guard let loaded = try? decoder.decode(T.self, from: data) else {
             fatalError("Failed to decode \(file) in bundle.")
         }
         return loaded
