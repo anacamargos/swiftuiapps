33fe25ea6c2743a4f97eda930330ba3c62379bac
anacamargos <alcvlara@sga.pucminas.br>
2022-07-07 09:11:51 -0300

    Creating new project

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Ana Leticia Camargos on 07/07/22.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
