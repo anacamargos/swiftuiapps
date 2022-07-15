cacb76194a3b5ff1be2f1aa327fb164982183c67
anacamargos <alcvlara@sga.pucminas.br>
2022-07-15 10:30:30 -0300

    Creating new project

---
+++ Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Ana Leticia Camargos on 15/07/22.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
