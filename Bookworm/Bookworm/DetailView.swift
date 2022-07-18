493e92b698bdf72453f2f55315fa2fa5338147e7
anacamargos <alcvlara@sga.pucminas.br>
2022-07-18 08:56:01 -0300

    Using an alert to pop a NavigationLink programmatically

--- Bookworm/Bookworm/DetailView.swift
+++ Bookworm/Bookworm/DetailView.swift
@@ -12,13 +12,17 @@ struct DetailView: View {
     
     let book: Book
     
+    @Environment(\.managedObjectContext) var moc
+    @Environment(\.dismiss) var dismiss
+    @State private var showingDeleteAlert = false
+    
     var body: some View {
         ScrollView {
             ZStack(alignment: .bottomTrailing) {
                 Image(book.genre ?? "Fantasy")
                     .resizable()
                     .scaledToFit()
-
+                
                 Text(book.genre?.uppercased() ?? "FANTASY")
                     .font(.caption)
                     .fontWeight(.black)
@@ -32,22 +36,41 @@ struct DetailView: View {
             Text(book.author ?? "Unknown author")
                 .font(.title)
                 .foregroundColor(.secondary)
-
+            
             Text(book.review ?? "No review")
                 .padding()
-
+            
             RatingView(rating: .constant(Int(book.rating)))
                 .font(.largeTitle)
         }
         .navigationTitle(book.title ?? "Unknown Book")
         .navigationBarTitleDisplayMode(.inline)
+        .alert("Delete book", isPresented: $showingDeleteAlert) {
+            Button("Delete", role: .destructive, action: deleteBook)
+            Button("Cancel", role: .cancel) { }
+        } message: {
+            Text("Are you sure?")
+        }
+        .toolbar {
+            Button {
+                showingDeleteAlert = true
+            } label: {
+                Label("Delete this book", systemImage: "trash")
+            }
+        }
+    }
+    
+    func deleteBook() {
+        moc.delete(book)
+        try? moc.save()
+        dismiss()
     }
 }
 
 struct DetailView_Previews: PreviewProvider {
     
     static let moc = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
-
+    
     static var previews: some View {
         let book = Book(context: moc)
         book.title = "Test book"
